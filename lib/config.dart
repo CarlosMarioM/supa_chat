e4def21a2b1ed2aaa3b9f8b7fcd327e0275f72b8
Mario Mederos <carlosmariomederos1@gmail.com>
2025-03-26 04:14:10 -0500

    fixing supabase keys web

---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+class Config {
+  static const String supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const String supabaseAnonKey =
+      String.fromEnvironment('SUPABASE_ANON_KEY');
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:supa_chat/view/splash_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'config.dart';
 import 'constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: ".env");
-
-  await Supabase.initialize(
-    url: dotenv.env['URL'] ?? '',
-    anonKey: dotenv.env['SUPA_KEY'] ?? '',
-  );
+  if (kIsWeb) {
+    await Supabase.initialize(
+      url: Config.supabaseUrl,
+      anonKey: Config.supabaseAnonKey,
+    );
+  } else {
+    await dotenv.load(fileName: ".env");
+    await Supabase.initialize(
+      url: dotenv.env['URL'] ?? '',
+      anonKey: dotenv.env['SUPA_KEY'] ?? '',
+    );
+  }
 
   runApp(const MyApp());
 }

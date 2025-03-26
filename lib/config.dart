22e2974aa691395173917d1482dfe3f2e95b92bc
Mario Mederos <carlosmariomederos1@gmail.com>
2025-03-26 14:10:58 -0500

    hardcoded values

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,7 @@
 class Config {
-  static const String supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const String supabaseUrl =
+      'https://roqustftdtsdgldpobbe.supabase.co'; //String.fromEnvironment('SUPABASE_URL');
   static const String supabaseAnonKey =
-      String.fromEnvironment('SUPABASE_ANON_KEY');
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcXVzdGZ0ZHRzZGdsZHBvYmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MjkwNDMsImV4cCI6MjA1ODUwNTA0M30.gQKuomBvbYLFrX37wM8_Xt5awfSwNGjQwX2kA7OYe1w';
+  //String.fromEnvironment('SUPABASE_ANON_KEY');
 }
